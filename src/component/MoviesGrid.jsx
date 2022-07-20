d20ba6cb6602353f7f90ba7c27dea9898edf18e3
glorialunar <glorialunar2509@gmail.com>
2022-07-20 20:30:08 -0300

    Creando un hook para que el search traiga las peliculas que se le indica y luego, al volver a home, se blanquee el campo

--- src/component/MoviesGrid.jsx
+++ src/component/MoviesGrid.jsx
@@ -1,23 +1,34 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import { useQuery } from "../hooks/useQuery";
 import { httpGet } from "../utils/httpClient";
 import { MovieCard } from "./MovieCard";
 import styles from "./MoviesGrid.module.css";
 import { Spinner } from "./Spinner";
 
+
+
 export function MoviesGrid(){
     const [ isLoading, setIsLoading ] = useState(true);
-    const [movies, setMovies] = useState([]);
-    
+    const [ movies, setMovies ] = useState([]);
+    const query = useQuery();
+    const search = query.get("search");
+
+    //Este efecto por defecto se cargará una primera vez completa, si se le pasa un parámetro por search, entonces va a traer solo las movies que se le indique en el panel de busqueda.
+
     useEffect(() => {
         setIsLoading(true);
-        httpGet("/discover/movie")
+        const searchUrl = search 
+            ? "/search/movie?query=" + search
+            : "/discover/movie";
+
+        httpGet(searchUrl)
             .then(data => {
                 setMovies(data.results);
                 setIsLoading(false);
         })
 
-    }, []);
+    }, [search]);
 
     if(isLoading){
         return <Spinner/>
--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,11 +1,20 @@
 import styles from "./Search.module.css";
 import { GoSearch } from 'react-icons/go';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate  } from "react-router-dom";
+import { useQuery } from "../hooks/useQuery";
 
 export function Search() {
+    const query = useQuery();
+    const search = query.get("search");
     const [searchText, setSearchText] = useState("");
     const navigate = useNavigate();
+
+    //Este hook va a permitir que se "limpie" el input luego de hacer una búsqueda y regresar a Home. Search, cuando no hay nada en la query, es null y el value del input nunca puede ser null. Es por eso que se implementa el || ""
+    useEffect(() => {
+        setSearchText(search || "");
+    }, [search]);
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
